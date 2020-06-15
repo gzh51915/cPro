5ccad8d104dab45eeee85031d1fa8475ca0a3f3e
muttou-24 <1284987774@qq.com>
2020-06-15 16:29:20 +0800

    add:AdminLogin API

---
+++ SF-admin-server/modules/AdminModule.js
@@ -0,0 +1,24 @@
+//用户
+const mongoose=require('mongoose')
+const md5 = require('blueimp-md5')
+
+const adminSchema = new mongoose.Schema({
+    username:{type:String,required:true},
+    password:{type:String,required:true},
+    phone:String,
+    email:String,
+    crea_time:{type:Number,default:Date.now},
+})
+
+const AdminModule =mongoose.model('admins',adminSchema)
+
+AdminModule.findOne({username:'admin'}).then(user=>{
+    if(!user){
+        AdminModule.create({username:'admin',password:md5('admin')}).then(res=>{
+            console.log('初始化用户：用户名：admin 密码：admin');
+            
+        })
+    }
+})
+
+module.exports =AdminModule
--- SF-admin-server/routers/index.js
+++ SF-admin-server/routers/index.js
@@ -1,1 +1,27 @@
-//看看
+const express =require('express')
+const md5 =require('blueimp-md5')
+
+const AdminModule=require('../modules/AdminModule')
+
+//得到路由器对象
+const router = express.Router()
+
+
+//管理员登录
+router.post('/admin/login',(req,res)=>{
+    const {username,password}=req.body
+    AdminModule.findOne({username,password:md5(password)}).then(admin=>{
+        if(admin){
+            res.cookie('adminid',admin._id,{maxAge:1000*60*60*24})
+            res.send({status:0,data:admin})
+        }else{
+            res.send({status:1,msg:'用户名或密码不正确！'})
+        }
+    }).catch(error=>{
+        console.log('登录异常',error);
+        res.send({status:1,msg:'登录异常，请重新尝试'})
+        
+    })
+})
+
+module.exports=router
