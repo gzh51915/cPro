ea096013696502e33fad62a1132f99d427bc6e8d
LeverLian <283714955@qq.com>
2020-06-18 20:44:35 +0800

    merge:移动端合并出错修改

--- cpro/src/index.js
+++ cpro/src/index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -17,28 +15,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-=======
-=======
->>>>>>> f9e48635cb97de0dce01b512dd9d9c6b45da8454
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'lib-flexible'
-import App from './App';
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
-<<<<<<< HEAD
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-<<<<<<< HEAD
->>>>>>> 0255f11e21487e1c983ee9b4fbc71e86fb1c8a95
-=======
->>>>>>> f9e48635cb97de0dce01b512dd9d9c6b45da8454
-=======
-
->>>>>>> a9df481fb268f0c3a689e360169e66312c4186fd
