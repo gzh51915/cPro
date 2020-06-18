620fa6f1f96903d8413035fca8db1f1fced7d50d
zhonggege <861167509@qq.com>
2020-06-18 20:25:13 +0800

    merge:package.json冲突解决

--- cpro/src/App.js
+++ cpro/src/App.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      cPro
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from 'react';
 import Tabbar from './component/Tabbar'
 import '../src/assets/css/index.css'
@@ -24,4 +11,3 @@ function App() {
 }
 
 export default App;
->>>>>>> a9df481fb268f0c3a689e360169e66312c4186fd
